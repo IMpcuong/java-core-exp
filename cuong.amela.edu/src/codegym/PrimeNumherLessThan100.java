727f63c8d5f27c067abad66ff36e196b5c1cf2e2
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-06-30 08:38:57 +0700

    Create PrimeNumherLessThan100.java

---
+++ cuong.amela.edu/src/codegym/PrimeNumherLessThan100.java
@@ -0,0 +1,20 @@
+package codegym;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static boolean checkPrime(int number) {
+        for (int i = 2; i <= (int) number/2; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        for (int num = 2; num <= 100; num++) {
+            if (checkPrime(num)) System.out.printf("%d is prime number\n", num);
+        }
+    }
+}
