aa4f82c1bc1ef41c854bcca4e97d768b3ee905a3
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-07-03 10:54:42 +0700

    Comparator shape

---
+++ cuong.amela.edu/src/codegym/oop/abstraction/TestShape.java
@@ -0,0 +1,29 @@
+package codegym.abstraction;
+
+public class TestShape {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
