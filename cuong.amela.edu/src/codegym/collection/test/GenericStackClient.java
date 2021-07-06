72bec3638cf9ce565b1a0849402a2eb5061d81c8
CuongTrinh1280 <58588543+CuongTrinh1280@users.noreply.github.com>
2021-07-06 23:53:15 +0700

    generic stack

---
+++ cuong.amela.edu/src/codegym/collection/test/GenericStackClient.java
@@ -0,0 +1,43 @@
+package codegym.collection.test;
+
+import codegym.collection.source.GenericStack;
+
+public class GenericStackClient {
+
+    private static void stackOfIStrings() {
+        GenericStack<String> stack = new GenericStack();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("1.1 Size of stack after push operations: " + stack.size());
+        System.out.printf("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations : " + stack.size());
+    }
+
+    private static void stackOfIntegers() {
+        GenericStack<Integer> stack = new GenericStack();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("2.1. Size of stack after push operations: " + stack.size());
+        System.out.printf("2.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+        System.out.println("\n3.3 Size of stack after pop operations : " + stack.size());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfIStrings();
+    }
+}
