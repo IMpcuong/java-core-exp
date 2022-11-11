61f1cc5dcf5923205cf53c57787f3cb439f665ff
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2022-11-11 08:45:09 +0700

    Update FizzBuzz.java

--- cuong.amela.edu/src/codegym/refactor/source/FizzBuzz.java
+++ cuong.amela.edu/src/codegym/refactor/source/FizzBuzz.java
@@ -2,14 +2,11 @@ package codegym.refactor.source;
 
 public class FizzBuzz {
     public static String fizzBuzz(int number){
-        if(number % 3 == 0 && number % 5 == 0)
-            return "FizzBuzz";
+        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
 
-        if(number % 3 == 0)
-            return "Fizz";
+        if (number % 3 == 0) return "Fizz";
 
-        if(number % 5 == 0)
-            return "Buzz";
+        if (number % 5 == 0) return "Buzz";
 
         return number + "";
     }
