2ee1b872fc3d6b699ca67421027f0d4d07fb3003
IMpossible <58588543+IMpcuong@users.noreply.github.com>
2022-11-11 08:49:40 +0700

    Update PrimeNumherLessThan100.java

--- cuong.amela.edu/src/codegym/PrimeNumherLessThan100.java
+++ cuong.amela.edu/src/codegym/PrimeNumherLessThan100.java
@@ -11,7 +11,6 @@ public class PrimeNumber {
         return true;
     }
 
-
     public static void main(String[] args) {
         for (int num = 2; num <= 100; num++) {
             if (checkPrime(num)) System.out.printf("%d is prime number\n", num);
